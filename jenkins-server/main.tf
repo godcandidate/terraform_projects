58a181c719d38c8232bbce144e6353362eadf3d6
godcandidate <godcandidate101@gmail.com>
2025-02-17 10:56:13 +0000

    fix(terraform): Assign public subnet and public IP to Jenkins server

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -6,12 +6,41 @@ resource "aws_vpc" "jenkins_vpc" {
   }
 }
 
+# Create an Internet Gateway
+resource "aws_internet_gateway" "jenkins_igw" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  tags = {
+    Name = "Jenkins-Internet-Gateway"
+  }
+}
+
+# Create a public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins_igw.id
+  }
+
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+# Associate the public route table with the subnet
+resource "aws_route_table_association" "public_subnet_assoc" {
+  subnet_id      = aws_subnet.jenkins_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 # Create a subnet
 resource "aws_subnet" "jenkins_subnet" {
   vpc_id     = aws_vpc.jenkins_vpc.id
   cidr_block = var.subnet_cidr
   tags = {
-    Name = "Jenkins-PublicSubnet"
+    Name = "Jenkins-Subnet"
   }
 }
 
@@ -51,11 +80,12 @@ resource "aws_security_group" "jenkins_sg" {
 
 # Launch an EC2 instance
 resource "aws_instance" "jenkins_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = aws_subnet.jenkins_subnet.id
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.jenkins_subnet.id
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  associate_public_ip_address = true # Ensure the instance gets a public IP
 
   user_data = file("${path.module}/userdata.sh")
 
